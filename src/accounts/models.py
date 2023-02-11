81d810c832e23acd23861be3d6402895653fec77
Almas Bekbayev <almas.bekbayev@gmail.com>
2023-02-11 12:02:27 +0600

    Add a custom user model

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,3 +1,6 @@
-from django.db import models  # noqa: F401
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+# Django highly recommended to set up a custom user model
+class User(AbstractUser):
+    pass
