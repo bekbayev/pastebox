b42b6f88b209b332731debc634552313a586b12e
Almas Bekbayev <almas.bekbayev@gmail.com>
2023-02-14 09:00:57 +0600

    Add a test to save and retrieve a Snippet model

--- src/snippets/tests.py
+++ src/snippets/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase  # noqa: F401
+from datetime import timedelta
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Snippet
+
+User = get_user_model()
+
+
+class SnippetModelTest(TestCase):
+    def test_save_and_retrieve(self) -> None:
+        """
+        The Snippet is saved in the database with the specified values
+        and is taken from the database with the same values.
+        """
+        user = User.objects.create()
+        expiration = timezone.now() + timedelta(days=21)
+        Snippet.objects.create(
+            author=user,
+            title="The Python code",
+            body="print('Need more tests!')",
+            syntax="python",
+            expiration=expiration,
+            url="4pT5hi6s",
+        )
+        self.assertEquals(Snippet.objects.count(), 1)
+        snippet = Snippet.objects.last()
+        self.assertEquals(snippet.author, user)
+        self.assertEquals(snippet.title, "The Python code")
+        self.assertEquals(snippet.body, "print('Need more tests!')")
+        self.assertEquals(snippet.syntax, "python")
+        self.assertEquals(snippet.expiration, expiration)
+        self.assertEquals(snippet.url, "4pT5hi6s")
