14838705b03eb33163c2e6a8c264dea7eff74427
Almas Bekbayev <almas.bekbayev@gmail.com>
2023-02-14 18:28:04 +0600

    Add extra description to make_random_string

--- src/snippets/utils.py
+++ src/snippets/utils.py
@@ -7,7 +7,9 @@ def make_random_string(length: int, chars: str = None) -> str:
 
     If no chars are given, the value will be a string of digits and
     ASCII letters.
-    A negative integer for length will return an empty string.
+    A negative integer for `length` will return an empty string.
+    Repeated characters in `chars` have an increased chance of passing
+    into the resulting string.
     """
     chars = chars or f"{ascii_letters}{digits}"
     return "".join(choice(chars) for _ in range(length))
