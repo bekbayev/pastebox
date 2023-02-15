6cb70fcc9a6612371730163613f6b6e130e50100
Almas Bekbayev <almas.bekbayev@gmail.com>
2023-02-15 09:14:03 +0600

    Add static files support

--- src/config/settings.py
+++ src/config/settings.py
@@ -115,6 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- src/config/urls.py
+++ src/config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -6,3 +8,6 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")),
     path("", include("snippets.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
