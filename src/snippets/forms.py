cc2904d345a51c1924d74668c2ffb5eebfb52fa2
Almas Bekbayev <almas.bekbayev@gmail.com>
2023-02-15 13:28:38 +0600

    Update SnippetForm using crispy-forms

--- src/snippets/forms.py
+++ src/snippets/forms.py
@@ -1,6 +1,10 @@
 from datetime import timedelta
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Div, Layout, Submit
+
 from django import forms
+from django.urls import reverse
 from django.utils import timezone
 
 from .models import Snippet
@@ -22,7 +26,33 @@ class SnippetForm(forms.ModelForm):
     class Meta:
         model = Snippet
         fields = ("title", "body", "syntax", "expiration")
-        widgets = {"body": forms.Textarea()}
+        widgets = {"body": forms.Textarea(attrs={"rows": 25})}
+        labels = {
+            "body": False,
+            "syntax": "Syntax highlighting",
+        }
+
+    @property
+    def helper(self) -> FormHelper:
+        helper = FormHelper()
+        self.fields["title"].initial = ""  # set html attr: value
+        helper.form_method = "POST"
+        helper.form_action = reverse("snippets:index")
+        helper.form_class = "row"
+        helper.layout = Layout(
+            Div("body", css_class="col-8"),
+            Div(
+                Div(Div("title", css_class="col"), css_class="row"),
+                Div(
+                    Div("syntax", css_class="col"),
+                    Div("expiration", css_class="col"),
+                    css_class="row",
+                ),
+                Submit("create", "Create", css_class="btn btn-success"),
+                css_class="col-4",
+            ),
+        )
+        return helper
 
     def clean_expiration(self) -> timezone.datetime | None:
         """Returns datetime object with time limit otherwise None.
