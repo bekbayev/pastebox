1b24faf2db5d489a987b2e5322f493e2522b7d67
Almas Bekbayev <almas.bekbayev@gmail.com>
2023-02-15 20:26:20 +0600

    Add SnippetDetailView to URL dispatcher

--- src/snippets/urls.py
+++ src/snippets/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import SnippetCreateView
+from .views import SnippetCreateView, SnippetDetailView
 
 app_name = "snippets"
 urlpatterns = [
     path("", SnippetCreateView.as_view(), name="index"),
+    path("<str:url>/", SnippetDetailView.as_view(), name="snippet_detail"),
 ]
