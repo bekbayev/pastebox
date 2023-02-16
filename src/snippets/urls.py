5f5453ce79f212340054dc89132ecb8ae9583288
Almas Bekbayev <almas.bekbayev@gmail.com>
2023-02-16 07:50:18 +0600

    Add SnippetDeleteView to URL dispatcher

--- src/snippets/urls.py
+++ src/snippets/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 
-from .views import SnippetCreateView, SnippetDetailView, SnippetUpdateView
+from .views import (
+    SnippetCreateView,
+    SnippetDeleteView,
+    SnippetDetailView,
+    SnippetUpdateView,
+)
 
 app_name = "snippets"
 urlpatterns = [
     path("", SnippetCreateView.as_view(), name="index"),
     path("<str:url>/", SnippetDetailView.as_view(), name="snippet_detail"),
     path("<str:url>/edit/", SnippetUpdateView.as_view(), name="snippet_edit"),
+    path("<str:url>/delete/", SnippetDeleteView.as_view(), name="snippet_delete"),
 ]
