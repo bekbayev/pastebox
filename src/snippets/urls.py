a0a5014d3df257d219b0da296ae92b81af8b3dc3
Almas Bekbayev <almas.bekbayev@gmail.com>
2023-02-16 07:17:35 +0600

    Add SnippetUpdateView to URL dispatcher

--- src/snippets/urls.py
+++ src/snippets/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import SnippetCreateView, SnippetDetailView
+from .views import SnippetCreateView, SnippetDetailView, SnippetUpdateView
 
 app_name = "snippets"
 urlpatterns = [
     path("", SnippetCreateView.as_view(), name="index"),
     path("<str:url>/", SnippetDetailView.as_view(), name="snippet_detail"),
+    path("<str:url>/edit/", SnippetUpdateView.as_view(), name="snippet_edit"),
 ]
